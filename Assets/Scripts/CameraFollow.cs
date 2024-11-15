5bf294b79c412ec9614468caa93f9165611ac2f9
nicola-baechi <nicola.baechi@outlook.com>
2024-11-16 00:21:36 +0100

    init commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float followSpeed = 2f;
+    public Transform target;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 updatedPosition = new Vector3(transform.position.x, target.position.y, -10f);
+        transform.position = Vector3.Slerp(transform.position, updatedPosition, followSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float horizontalMoveSpeed = 5f;
+    public float verticalMoveSpeed = 1f;
+    
+    public Rigidbody2D rb;
+    
+    private Vector2 moveDirection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        
+        moveDirection = new Vector2(moveX, 0).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveDirection.x * horizontalMoveSpeed, verticalMoveSpeed);
+    }
+}
