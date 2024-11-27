c9081f6f8eec91ac9a7b9d175aef7d885affada6
athavanranganathan <athavan.ranganathan@stud.hslu.ch>
2024-11-27 14:42:32 +0100

    lore scene created

---
+++ Assets/Scripts/UI/Dialogue.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+    
+    private int index;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        StartDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+            
+        }
+    }
+    
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypteLine());
+    }
+    
+    IEnumerator TypteLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+    
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypteLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
